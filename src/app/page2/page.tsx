16bc00a851bf9d0b5775713689142a72bb5ef04c
David Lopez <sscampusmty@gmail.com>
2025-02-17 12:42:06 -0600

    Page 2

--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -1,5 +1,11 @@
-export default function Page2() {
+import Navbar from "@/components/Navbar";
+
+export default function Page1() {
     return (
-        <div></div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <Navbar active="Page2"/>
+
+            <h1 className="text-4xl font-bold">En Construcción</h1>
+        </div>
     );
 };
